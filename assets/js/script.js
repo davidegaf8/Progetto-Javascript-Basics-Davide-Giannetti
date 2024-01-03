d223279ccf18211cef931b6e330c2bc9a0f8a21e
davidegaf8 <134536189+davidegaf8@users.noreply.github.com>
2024-01-03 17:00:30 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+let toggleBtn = document.querySelector(".toggle");
+let indicator = document.querySelector(".ball");
+let counter = document.querySelector(".count");
+let decreaseBtn = document.querySelector(".decrease-btn");
+let resetBtn = document.querySelector(".reset-btn");
+let increaseBtn = document.querySelector(".increase-btn");
+
+let username = prompt("Hello,what's your name?");
+
+let welcome = document.createElement("h1");
+welcome.textContent = `Welcome ${username},thanks for using my counter`;
+if (username ==="") {
+  welcome.textContent = "Welcome user,thanks for using my counter"
+}
+welcome.classList.add("welcome");
+document.body.appendChild(welcome)
+
+let count = 0;
+
+
+toggleBtn.addEventListener("click",()=>{
+  document.body.classList.toggle("dark");
+  indicator.classList.toggle("dark");
+  welcome.classList.toggle("dark");
+  toggleBtn.classList.toggle("dark")
+})
+
+decreaseBtn.addEventListener("click",()=>{
+  count--;
+  counter.textContent = count;
+})
+
+resetBtn.addEventListener("click",()=>{
+  count=0;
+  counter.textContent = count;
+})
+
+increaseBtn.addEventListener("click",()=>{
+  count++;
+  counter.textContent = count;
+})
+
+
+
+
+
+
+
